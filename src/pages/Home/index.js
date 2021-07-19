fe777bec005f56cf0db9b7db117b445bae0930e6
Lucas Caldeira <mrrealcaldeira@gmail.com>
2021-07-19 19:55:53 -0300

    tela home 1/3 pronta

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, BackgroundImage, BtnStartNow, TitleButton } from './styles';
+
+import Background from '../../assets/background-home.jpg';
+import arrowRight from '../../assets/arrow-right.png';
 
 export default function Home() {
-  return <Container>Teste</Container>;
+  function handleStartNow() {
+    alert('Teste');
+  }
+
+  return (
+    <Container>
+      <BackgroundImage src={Background} />
+
+      <BtnStartNow onClick={handleStartNow}>
+        <TitleButton>Start now</TitleButton>
+      </BtnStartNow>
+    </Container>
+  );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,52 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: red;
+  flex: 1;
+`;
+
+export const BackgroundImage = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+
+export const BtnStartNow = styled.button`
+  position: fixed;
+
+  width: 20%;
+
+  bottom: 21px;
+  right: 28px;
+  background-color: #f12711;
+
+  border-radius: 10px;
+
+  &:hover {
+    box-shadow: -8px 8px 0 0 #000;
+    transform: translate(8px, -8px);
+    -webkit-transform: translate(8px, -8px);
+    -moz-transform: translate(8px, -8px);
+    -ms-transform: translate(8px, -8px);
+    -o-transform: translate(8px, -8px);
+
+    border: solid 2px #000;
+    border-radius: 8px;
+    margin: 7px 0;
+    cursor: pointer;
+    transition: all ease-in 0.3s;
+    -webkit-transition: all ease-in 0.3s;
+    -moz-transition: all ease-in 0.3s;
+    -ms-transition: all ease-in 0.3s;
+    -o-transition: all ease-in 0.3s;
+  }
+`;
+
+export const TitleButton = styled.h1`
+  color: white;
+  font-size: 3vh;
+`;
+
+export const Arrow = styled.img`
+  width: 55px;
 `;
